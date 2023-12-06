b6d61b0ce87b19f6ba46d1dcd39fa4ab9259dbdb
Mihai Solescu <mihai.solescu@outlook.com>
2023-12-06 15:20:42 +0100

    Button functionality

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,26 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.awt.Desktop;
+import java.awt.Label;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -30,12 +36,24 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private final Desktop desktop = Desktop.getDesktop();
+    //private final Desktop desktop = Desktop.getDesktop();
 
     int dataCount = 0; //used to count Data objects
     int removeCount = 1; //number of removed Data objects offset by 1
     ArrayList<Data> dataList = new ArrayList<Data>();
+    ObservableList<String> columnList;
 
+    private void refreshColumnList(FlowPane filters){ //has a problem when the same column names appear more than once - considers them as different.
+        ArrayList<String> temp = new ArrayList<String>();
+        for(Data data : dataList)
+            for(String s : data.columnNames)
+                temp.add(s);
+        columnList = FXCollections.observableArrayList(temp);
+        ChoiceBox<String> xAxis = new ChoiceBox<String>(columnList);
+        xAxis.setValue("------------");
+        ChoiceBox<String> yAxis = new ChoiceBox<String>(columnList);
+        yAxis.setValue("------------");
+    }
 
     @Override
     public void start(Stage stage) {
@@ -49,59 +67,26 @@ public class Main extends Application {
         stage.setWidth(1080);
         stage.setHeight(720); 
         //stage.setResizable(false);
-        //stage.setX(300);
-        //stage.setY(100);
 
-        /*
-        ///Text
-        Text text = new Text(20.0, 10.0, "Text");
-        text.setFont(Font.font("Segoe UI", 13));
-        text.setFill(Color.BLUEVIOLET);
+        ///Root
+        BorderPane root = new BorderPane();
 
-        ///Line
-        Line line = new Line();
-        line.setStartX(100);
-        //line.setStartY(200);
-        line.setEndX(200);
-        //line.setEndY(200);
-        line.setStrokeWidth(5);
-        line.setOpacity(.75);
-
-        ///Rectangle
-        Rectangle rect = new Rectangle();
-        rect.setX(50);
-        rect.setY(50);
-        rect.setWidth(100);
-        rect.setHeight(100);
-        rect.setFill(Color.BLUEVIOLET);
-
-        ///Polygon
-        Polygon poly = new Polygon();
-        poly.getPoints().addAll(
-            200.0, 100.0,
-            200.0, 200.0,
-            100.0, 150.0
-        );
-        poly.setFill(Color.BLACK);
-
-        ///Circle
-        Circle circle = new Circle();
-        circle.setCenterX(100.0);
-        circle.setCenterY(200.0);
-        circle.setRadius(50.0);
-        circle.setFill(Color.GREEN);
-
-        ///ImageView
-        ImageView imgView = new ImageView(icon);
-        imgView.setX(200.0);
-        imgView.setY(200);
-        imgView.setFitWidth(400);
-        imgView.setFitHeight(400);
-
-        ///Group
-        Group group = new Group();
-        group.getChildren().addAll(text, line, rect, poly, circle, imgView);
-        */
+        ///Sandbox Pane
+        BorderPane sandbox = new BorderPane();
+        sandbox.setStyle("-fx-background-color: #fff5ee");
+        //sandbox.setAlignment(graph, Pos.CENTER);
+        //Sandbox Filter Bar FlowPane
+        FlowPane filters = new FlowPane(10, 10);
+        //Label xAxisLab = new Label("X Axis: ");
+        ComboBox<String> xAxis = new ComboBox<String>();
+        xAxis.setValue("----------");
+        //Label yAxisLab = new Label("X Axis: ");
+        ComboBox<String> yAxis = new ComboBox<String>();
+        yAxis.setValue("----------");
+        filters.getChildren().addAll(xAxis, yAxis);
+        filters.getChildren().remove(0);
+        filters.getChildren().add(0, xAxis);
+        sandbox.setTop(filters);
 
         ///Side Menu
         VBox vbox = new VBox();
@@ -112,17 +97,14 @@ public class Main extends Application {
         Button button1 = new Button(BUTTON1_TEXT);
         button1.setPrefWidth(vbox.getPrefWidth());
         button1.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
-
         final String BUTTON2_TEXT = "Step 1 Findings";
         Button button2 = new Button(BUTTON2_TEXT);
         button2.setPrefWidth(vbox.getPrefWidth());
         button2.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
-
         final String BUTTON3_TEXT = "Step 2 Findings";
         Button button3 = new Button(BUTTON3_TEXT);
         button3.setPrefWidth(vbox.getPrefWidth());
         button3.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
-
         final String BUTTON4_TEXT = "Steps 3 & 4 Findings";
         Button button4 = new Button(BUTTON4_TEXT);
         button4.setPrefWidth(vbox.getPrefWidth());
@@ -136,36 +118,49 @@ public class Main extends Application {
                         button2.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button3.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button4.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
+                        root.setCenter(sandbox);
+                        stage.show();
                         break;
                     case BUTTON2_TEXT:
                         button1.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button2.setStyle("-fx-background-color: #666666; -fx-text-fill: white;");
                         button3.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button4.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
+                        BorderPane f = new BorderPane();
+                        f.setStyle("-fx-background-color: #444444");
+                        root.setCenter(f);
+                        stage.show();
                         break;
                     case BUTTON3_TEXT:
                         button1.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button2.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");    
                         button3.setStyle("-fx-background-color: #666666; -fx-text-fill: white;");
                         button4.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
-                        
+                        BorderPane g = new BorderPane();
+                        g.setStyle("-fx-background-color: #333333");
+                        root.setCenter(g);
+                        stage.show();
                         break;
                     case BUTTON4_TEXT:
                         button1.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button2.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button3.setStyle("-fx-background-color: #444444; -fx-text-fill: white;");
                         button4.setStyle("-fx-background-color: #666666; -fx-text-fill: white;");
+                        BorderPane h = new BorderPane();
+                        h.setStyle("-fx-background-color: #222222");
+                        root.setCenter(h);
+                        stage.show();
                         break;
                     default:
                 }
             }
         };
-
         button1.setOnAction(sideMenuHandler);
         button2.setOnAction(sideMenuHandler);
         button3.setOnAction(sideMenuHandler);
         button4.setOnAction(sideMenuHandler);
         vbox.getChildren().addAll(button1, button2, button3, button4);
+        ///End Side Menu
 
         vbox.addEventFilter(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e){
@@ -175,7 +170,7 @@ public class Main extends Application {
 
         ///Menu Bar
         MenuBar menuBar = new MenuBar();
-        Menu dataMenu = dataMenu(stage);
+        Menu dataMenu = dataMenu(stage, filters);
             
         Menu preferencesMenu = new Menu("Preferences");
         Menu fontSMenu = new Menu("Font");
@@ -194,16 +189,10 @@ public class Main extends Application {
         menu.getItems().addAll(histogramItem, piechartItem, piechartItem1, pieCHartItem2, pie);
         menuBar.getMenus().addAll(dataMenu, preferencesMenu, menu);
 
-        ///Sandbox Pane
-        BorderPane sandbox = new BorderPane();
-        //sandbox.setAlignment(graph, Pos.CENTER);
 
-
-        ///Root
-        BorderPane root = new BorderPane();
+        ///add panes to root
         root.setLeft(vbox);
         root.setTop(menuBar);
-        root.setCenter(sandbox);
        
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -232,26 +221,28 @@ public class Main extends Application {
      * @param stage
      * @return
      */
-    private Menu dataMenu(Stage stage){
+    private Menu dataMenu(Stage stage, FlowPane filters){
         Menu dataMenu = new Menu("Data");
         MenuItem addData = new MenuItem("add data");
         addData.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
-                dataImport(stage);
-                int dC = dataCount++;
-                Menu data = new Menu("data" + dC);
-                MenuItem rmData = new MenuItem("remove data");
-                rmData.setOnAction(new EventHandler<ActionEvent>(){
-                    public void handle(ActionEvent e){
-                        dataMenu.getItems().remove(data);
-                        dataList.remove(dataCount - removeCount++);
-                        //System.out.println(dataList.size());
-                    }
-                });
-                data.getItems().add(rmData);
-                dataMenu.getItems().add(data);
-                //System.out.println(dataList.size());
+                if(dataImport(stage, filters)){
+                    int dC = dataCount++;
+                    Menu data = new Menu("data" + dC);
+                    MenuItem rmData = new MenuItem("remove data");
+                    rmData.setOnAction(new EventHandler<ActionEvent>(){
+                        public void handle(ActionEvent e){
+                            dataMenu.getItems().remove(data);
+                            dataList.remove(dataCount - removeCount++);
+                            refreshColumnList(filters);
+                            //System.out.println(dataList.size());
+                        }
+                    });
+                    data.getItems().add(rmData);
+                    dataMenu.getItems().add(data);
+                    //System.out.println(dataList.size());
+                }
             }
         });
         dataMenu.getItems().addAll(addData);
@@ -262,7 +253,7 @@ public class Main extends Application {
      * Creates a menu used to import the data from a .csv file into the program.
      * @param stage
      */
-    private void dataImport(Stage stage){
+    private boolean dataImport(Stage stage, FlowPane filters){
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Text files", "*.csv");
         fileChooser.setTitle("Import Data");
@@ -270,9 +261,11 @@ public class Main extends Application {
         File file = fileChooser.showOpenDialog(stage);
         if (file != null) {
             dataList.add(new Data(file));
+            refreshColumnList(filters);
+            return true;
             //openFile(file);
         } else {
-            Data data = new Data("data/bugData.csv");
+            return false;
         }
     }
 }
