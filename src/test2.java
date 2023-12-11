3279ca94bcf5dc1450f2ba8b62b1ad0b55146042
Mihai Solescu <mihai.solescu@outlook.com>
2023-12-11 16:01:04 +0100

    Data Element Rework; Project Structure

--- src/test2.java
+++
@@ -1,8 +0,0 @@
-public class test2 {
-       public static void main(String[] args) {
-              Data combinedData = new Data(new AggregateData("src/data/CurrentGrades.csv", "src/data/StudentInfo.csv"));
-              for(String s : combinedData.columnNames){
-                     System.out.println(s);
-       }
-}
-}
